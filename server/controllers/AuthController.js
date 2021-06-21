1198125c921900c83ad675797308fa0e4d523b68
Richard Miller <richardmmill@gmail.com>
2021-06-21 11:56:51 -0400

    fixing server and endpoints/middleware

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,9 +1,11 @@
-const {User} = require('../models/')
+const {User} = require('../models')
 
 module.exports = {
     async register(req, res) {
+        console.log(req.body)
         try {
             const user = await User.create(req.body) 
+            // console.log(user)
             res.send(user.toJSON())
         } catch (err) {
             res.status(400).send({
--- server/policies/AuthenticationPolicy.js
+++ server/policies/AuthenticationPolicy.js
@@ -3,11 +3,13 @@ const Joi = require('joi');
 module.exports = {
     register (req, res, next) {
         const schema = Joi.object ({
-            email: Joi.string().email().required(),
+            email: Joi.string().email(),
             password: Joi.string().regex(
                 new RegExp('^[a-zA-Z0_9]{8,32}$')
-            ).required()
+            )
         })
+
+        console.log(req.body)
 
         const {error, value} = schema.validate(req.body)
 
