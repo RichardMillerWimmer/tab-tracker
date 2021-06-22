d1394a38a6a2ad47141fd691ef37f5d8be66a784
Richard Miller <richardmmill@gmail.com>
2021-06-22 13:29:31 -0400

    no vuetify, vue3 not compatable

--- src/main.js
+++ src/main.js
@@ -1,18 +1,5 @@
-// import Vue from 'vue'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import Vuetify from 'vuetify/lib'
-// import Vuetify from 'vuetify';
-// import 'vuetify/dist/vuetify.min.css';
-
-// Vue.use(Vuetify);
 
 createApp(App).use(router).mount('#app')
-
-// new Vue({
-//     el: '#app',
-//     router, 
-//     template: '<App/>',
-//     component: { App }
-// })
